0cabbda334d6854c808733c706983e5c26b77c0d
Guillaume Destuynder <kang+mozilla@insecure.ws>
2018-05-31 11:18:24 -0700

    tests for token authorization

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -3,6 +3,7 @@
 import json
 import unittest
 import os
+import time
 
 from unittest.mock import patch
 from authzero import AuthZero
@@ -29,3 +30,54 @@ class AuthZeroLibTest(unittest.TestCase):
         assert(ret[0].get('id') is not None)
         assert(ret[0].get('script') is not None)
         assert(ret[0].get('enabled') is True)
+
+    def test_get_bad_access_token(self):
+        config = {'client_id': 'AAAA', 'client_secret': 'BBBB', 'uri': 'localhost'}
+        az = AuthZero(config)
+        az.access_token = {}
+        with self.assertRaises(Exception) as context:
+            az._authorize(az.default_headers)
+        assert('InvalidAccessToken' == str(context.exception))
+
+    @patch.object(AuthZero, "get_access_token")
+    def test_get_good_access_token(self, mock_get_access_token):
+        fix_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data/access_token.json')
+        with open(fix_path) as fd:
+            fix = json.load(fd)
+
+        mock_get_access_token.return_value = fix
+        config = {'client_id': 'AAAA', 'client_secret': 'BBBB', 'uri': 'localhost'}
+        az = AuthZero(config)
+        az.access_token = az.get_access_token()
+        az.access_token_valid_until = time.time() + az.access_token.get('expires_in')
+        az._authorize(az.default_headers)
+
+    @patch.object(AuthZero, "get_access_token")
+    def test_get_auto_renew_access_token(self, mock_get_access_token):
+        fix_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data/access_token.json')
+        with open(fix_path) as fd:
+            fix = json.load(fd)
+
+        mock_get_access_token.return_value = fix
+        config = {'client_id': 'AAAA', 'client_secret': 'BBBB', 'uri': 'localhost'}
+        az = AuthZero(config)
+        az.access_token = az.get_access_token()
+        az.access_token_valid_until = 0
+        az.access_token_auto_renew = True
+        az._authorize(az.default_headers)
+
+    @patch.object(AuthZero, "get_access_token")
+    def test_expired_access_token(self, mock_get_access_token):
+        fix_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data/access_token.json')
+        with open(fix_path) as fd:
+            fix = json.load(fd)
+
+        mock_get_access_token.return_value = fix
+        config = {'client_id': 'AAAA', 'client_secret': 'BBBB', 'uri': 'localhost'}
+        az = AuthZero(config)
+        az.access_token = az.get_access_token()
+        az.access_token_valid_until = 0
+        az.access_token_auto_renew = False
+        with self.assertRaises(Exception) as context:
+            az._authorize(az.default_headers)
+        assert("('InvalidAccessToken', 'The access token has expired')" == str(context.exception))
