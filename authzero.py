fa846f2a326c37d8699434242c216d4a5c5ca613
Guillaume Destuynder <kang+mozilla@insecure.ws>
2018-05-31 11:18:06 -0700

    Add support for auto renewing access token

--- authzero.py
+++ authzero.py
@@ -68,6 +68,8 @@ class AuthZero(object):
         self.access_token = None
         self.access_token_scope = None
         self.access_token_valid_until = 0
+        self.access_token_auto_renew = False
+        self.access_token_auto_renew_leeway = 60
         self.conn = http.client.HTTPSConnection(config.uri)
         self.rules = []
 
@@ -418,7 +420,10 @@ class AuthZero(object):
     def _authorize(self, headers):
         if not self.access_token:
             raise Exception('InvalidAccessToken')
-        if self.access_token_valid_until < time.time():
+        if self.access_token_auto_renew:
+            if self.access_token_valid_until < time.time() + self.access_token_auto_renew_leeway:
+                self.access_token = self.get_access_token()
+        elif self.access_token_valid_until < time.time():
             raise Exception('InvalidAccessToken', 'The access token has expired')
 
         local_headers = {}
